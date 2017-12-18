395edf29dad127934b38ba77004c006f2ad8ca92
anlaochou@iweichan.com <anlaochou@iweichan.com>
2017-12-18 10:05:54 +0800

    clear

--- ccalendarlib/src/main/java/com/n/ccalendarlib/CalendarDateView.java
+++ ccalendarlib/src/main/java/com/n/ccalendarlib/CalendarDateView.java
@@ -40,28 +40,6 @@ public class CalendarDateView extends BaseCalendarView {
     private float downY;
     private float downX;
 
-//    private int startIndex = 0;
-//    private Mode mode = Mode.Month;
-//
-//    enum Mode {
-//        Week, Month
-//    }
-//
-//    public void setMode(Mode mode) {
-//        this.mode = mode;
-//        if (isMonthMode()) {
-//            maxTable = 42;
-//            startIndex = 0;
-//        } else {
-//            maxTable = 7;
-//            startIndex = DateUtil.getWeekOfMonth(currentMonthCalendar) * 7;
-//        }
-//    }
-//
-//    public boolean isMonthMode(){
-//        return mode == Mode.Month;
-//    }
-
     private List<Calendar> scheduleDate = new ArrayList<>();
 
     public CalendarDateView(Context context) {
